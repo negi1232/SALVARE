130119b2350fc2834ed1b16363c9bf8f3135ac01
yawn <69970183+yawn-c111@users.noreply.github.com>
2023-06-06 02:27:36 +0900

    feat: move startWorkButton to Map.js

--- salvare/src/pages/detail/home.js
+++ salvare/src/pages/detail/home.js
@@ -1,82 +1,79 @@
-import { useState, useEffect, useRef } from 'react';
-import { useParams } from "react-router-dom";
-import QRCodeComponent from './component/qr_code';
-import MapComponent from './component/Map';
+import { useState, useEffect, useRef } from 'react'
+import { useParams } from 'react-router-dom'
+import QRCodeComponent from './component/qr_code'
+import MapComponent from './component/Map'
 import { MapContainer, TileLayer } from 'react-leaflet'
 import './component/Map.css'
 function Detail_top(props) {
+  const [data, setData] = useState(null)
+  const [position, setPosition] = useState([0, 0])
 
+  const id = useParams()['id']
 
-    const [json, Setjson] = useState({})
-    const [data, setData] = useState(null);
-    const [position, setPosition] = useState([0,0]);
-
-    const id = useParams()["id"];
-
-    //初回のみ実行
-    useEffect(() => {
-        const get_variable = async () => {
-
-            if (await props.cont.isMetaMaskConnected()) {
-                const _data=await props.cont.get_garbage_can(id)
-                setData(_data);
-                setPosition([  parseInt(_data["lat"]["_hex"]) / 100000000000000 ,parseInt(_data["lot"]) / 100000000000000]);
-                // setPosition([ 30,30 ]);
-            }
-            //console.log([parseInt(data["lot"]) / 10000000000000, parseInt(data["lat"]) / 1000000000000000])
-        }
-        console.log("useEffect");
-        get_variable();
-    }, []);
-
-    if (data!=null) {
-
-        console.log(data);
-        console.log(position,"position");
-        console.log(id);
-        return (
-            <>
-                <div>
-                    住所<br />
-                    {data["location_address"]}
-                </div>
-                <div>
-                    回収前の重さ<br />
-                    {parseInt(data["amount"])}g
-                </div>
-                <div>
-                    ゴミ箱の最大容量<br />
-                    {parseInt(data["max_amount"])}g
-                </div>
-                <div>
-                    報酬<br />
-                    {parseInt(data["reward"])}slv
-                </div>
-                <div>
-                    依頼主<br />
-                    {data["owner"]}
-                </div>
-
-                <MapComponent
-                    position={position}
-                    location_address={data["location_address"]}
-                    amount={parseInt(data["amount"])}
-                    max_amount={parseInt(data["max_amount"])}
-                    reward={parseInt(data["reward"])}
-                    owner={parseInt(data["owner"])}
-                />
-
-                {/* <QRCodeComponent json={json} /> */}
-                <br />
-                <button type="button" class="btn btn-dark" onClick={() => { props.cont.sign(data.toString(), Setjson) }} >仕事を開始する</button>
-
-            </>
-        );
-    } else {
-        return (
-            <>
-            </>
-        );
+  //初回のみ実行
+  useEffect(() => {
+    const get_variable = async () => {
+      if (await props.cont.isMetaMaskConnected()) {
+        const _data = await props.cont.get_garbage_can(id)
+        setData(_data)
+        setPosition([
+          parseInt(_data['lat']['_hex']) / 100000000000000,
+          parseInt(_data['lot']) / 100000000000000
+        ])
+        // setPosition([ 30,30 ]);
+      }
+      //console.log([parseInt(data["lot"]) / 10000000000000, parseInt(data["lat"]) / 1000000000000000])
     }
+    console.log('useEffect')
+    get_variable()
+  }, [])
+
+  if (data != null) {
+    console.log(data)
+    console.log(position, 'position')
+    console.log(id)
+    return (
+      <>
+        <div>
+          住所
+          <br />
+          {data['location_address']}
+        </div>
+        <div>
+          回収前の重さ
+          <br />
+          {parseInt(data['amount'])}g
+        </div>
+        <div>
+          ゴミ箱の最大容量
+          <br />
+          {parseInt(data['max_amount'])}g
+        </div>
+        <div>
+          報酬
+          <br />
+          {parseInt(data['reward'])}slv
+        </div>
+        <div>
+          依頼主
+          <br />
+          {data['owner']}
+        </div>
+
+        <MapComponent
+          position={position}
+          location_address={data['location_address']}
+          amount={parseInt(data['amount'])}
+          max_amount={parseInt(data['max_amount'])}
+          reward={parseInt(data['reward'])}
+          owner={parseInt(data['owner'])}
+          const={props.cont}
+          data={data}
+        />
+      </>
+    )
+  } else {
+    return <></>
+  }
 }
-export default Detail_top;
+export default Detail_top
