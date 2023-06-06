edd64677bf87eb6fb5e64a8ddb1028410cc91566
CanIyu <35588557+CanIyu@users.noreply.github.com>
2023-06-06 13:45:56 +0900

    arranged files under the detail dir

--- salvare/src/pages/detail/component/PopupContent.js
+++ salvare/src/pages/detail/component/PopupContent.js
@@ -3,15 +3,15 @@ import React from 'react';
 const PopupContent = ({ pin }) => {
   return (
     <div>
-      <h4>Location: {pin.location_address}</h4>
-      <p>Amount: {pin.amount}</p>
-      <p>Max Amount: {pin.max_amount}</p>
-      <p>Reward: {pin.reward}</p>
-      <p>Owner: {pin.owner}</p>
-      <p>Client: {pin.client}</p>
-      <p>Client Longitude: {pin.client_longitude}</p>
-      <p>Client Latitude: {pin.client_latitude}</p>
-      <img src={pin.location_image} width={"200px"} alt="Location" />
+      <h4>Location: {pin.trashCan_location_address}</h4>
+      <p>Amount: {pin.trashCan_amount}</p>
+      <p>Max Amount: {pin.trashCan_max_amount}</p>
+      <p>Reward: {pin.trashCan_reward}</p>
+      <p>Owner: {pin.trashCan_owner}</p>
+      <p>Recycling Center: {pin.recyclingCenter}</p>
+      <p>Recycling Center Longitude: {pin.recyclingCenter_longitude}</p>
+      <p>Recycling Center Latitude: {pin.recyclingCenter_latitude}</p>
+      <img src={pin.trashCan_location_image} width={"200px"} alt="Location" />
     </div>
   );
 };
