cb5f728079343788bd8ea273a032876e8fbfa948
R N <ryosuke.geek@gmail.com>
2023-06-07 01:20:13 +0900

    現状復帰

--- salvare/src/pages/detail/component/ShowPins.js
+++ salvare/src/pages/detail/component/ShowPins.js
@@ -1,20 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import PopupContent from './PopupContent';
-
+import TrashCanChart from './TrashCanChart';
 import dummyData from './dummyData.json';
 
 const ShowPins = (props) => {
   const [pins, setPins] = useState([]);
-  const [rcPins, setRcPins] = useState([]);
-  const [mapCenter, ] = useState([35.658580, 139.701664]);
+  const [data, setData] = useState(null);
+  const [mapCenter, setMapCenter] = useState([35.658580, 139.700464]);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [active_pin,setActive_pin]=useState(null);
 
   const handleMarkerClick = () => {
     setIsModalOpen(true);
   };
-
+  
   // モーダルが閉じられた際のハンドラ
   const closeModal = () => {
     setIsModalOpen(false);
@@ -25,16 +25,13 @@ const ShowPins = (props) => {
   useEffect(() => {
     const get_variable = async () => {
       if (await props.cont.isMetaMaskConnected()) {
-        const _data = await props.cont.get_garbage_cans();
+        const _data = await props.cont.getTrashCans();
         setPins(_data);
       }
     };
+    console.log("useEffect");
     get_variable();
-  }, [props.cont]);
-
-  // fetch data from dummyData
-  useEffect(() => {
-    setRcPins(dummyData);
+    console.log(mapCenter)
   }, []);
 
   if (pins) {
@@ -52,25 +49,18 @@ const ShowPins = (props) => {
         {pins.map((pin,index) => (
           <Marker
             key={index}
-            position={
-              [parseFloat(pin["lat"]["_hex"] / 100000000000000),
-              parseFloat(pin["lot"]["_hex"] / 100000000000000)]}
+            position={[parseFloat(pin.trashCanLatitude._hex/100000000000000) ,parseFloat(pin.trashCanLongitude._hex/100000000000000)]}
             eventHandlers={{
               click: () => {
                 handleMarkerClick();
                 setActive_pin(pin);
+                console.log(pin);
               },
             }}
           >
           </Marker>
         ))}
-        {isModalOpen &&
-          (<PopupContent
-            pin={active_pin}
-            rcPins={rcPins}
-            cont={props.cont}
-            isOpen={isModalOpen}
-            closeModal={closeModal} />)}
+        {isModalOpen && (<PopupContent pin={active_pin} cont={props.cont} isOpen={isModalOpen} closeModal={closeModal}/>)}
       </MapContainer>
     );
   }
