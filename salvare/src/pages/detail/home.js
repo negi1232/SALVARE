e946b0d8a41de7fc8c641de4ba5d6531a3998d93
CanIyu <35588557+CanIyu@users.noreply.github.com>
2023-06-07 15:12:36 +0900

    fetched one's account balance on detail/home.js

--- salvare/src/pages/detail/home.js
+++ salvare/src/pages/detail/home.js
@@ -1,10 +1,16 @@
-import ShowPins from "./component/ShowPins";
 import React from 'react';
+import ShowPins from "./component/ShowPins";
 
 function DetailTop(props) {
+  // fetched one's account balance
+  const accountBlance = async () => {
+    console.log( parseInt((await props.cont.fetchAccountBalance())._hex) );
+  };
+  accountBlance()
+
   return (
     <>
-      <ShowPins cont={props.cont} />
+      <ShowPins cont={props.cont}/>
     </>
   );
 }
